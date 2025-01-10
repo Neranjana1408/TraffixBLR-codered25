5bbfa33d96852a7a57854cd63b7130c076b9fb21
Sreeram-Akhil <164717447+Sreeram-Akhil@users.noreply.github.com>
2025-01-10 10:43:23 +0530

    Update 3rd_person_view.ino

--- 3rd_person_view.ino
+++ 3rd_person_view.ino
@@ -2,48 +2,42 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// nRF24L01 Setup
-RF24 radio(9, 10); // CE, CSN
+RF24 radio(9, 10); 
 const byte address[6] = "00001";
 
-// Structure to receive GPS data
+
 struct Location {
-  double latitude;   // Latitude received from the transmitter
-  double longitude;  // Longitude received from the transmitter
+  double latitude;   
+  double longitude;
 };
 
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(9600);
 
-  // nRF24L01 Initialization
   if (!radio.begin()) {
     Serial.println("nRF24L01 not detected! Check connections.");
-    while (1); // Halt
+    while (1); 
   }
 
   radio.openReadingPipe(1, address);
   radio.setChannel(108);
   radio.setPALevel(RF24_PA_HIGH);
-  radio.setAutoAck(false); // Disable Auto-ACK
+  radio.setAutoAck(false); 
   radio.startListening();
 
   Serial.println("Receiver Ready");
 }
 
 void loop() {
-  // Check if there is data available from the transmitter
   if (radio.available()) {
     Location receivedData;
 
-    // Read the received GPS data
     radio.read(&receivedData, sizeof(receivedData));
 
-    // Print the received data to the Serial Monitor
     Serial.println("Received GPS data:");
     Serial.print("Latitude: ");
-    Serial.println(receivedData.latitude, 6); // Print latitude with 6 decimal places
+    Serial.println(receivedData.latitude, 6); 
     Serial.print("Longitude: ");
-    Serial.println(receivedData.longitude, 6); // Print longitude with 6 decimal places
+    Serial.println(receivedData.longitude, 6); 
   }
 }
